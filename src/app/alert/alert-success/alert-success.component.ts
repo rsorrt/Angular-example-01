464dd4b49210cfde109a785cd1f446888b187586
Lof <akumpum@gmail.com>
2021-05-30 18:15:47 +0200

    primo commiy

---
+++ src/app/alert/alert-success/alert-success.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-alert-success',
+    templateUrl: './alert-success.component.html',
+    styleUrls: ['.././alert.css']
+})
+export class AlertSuccessComponent{
+
+
+}
+
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+  //inline style
+  //styles: [ `
+  //  h3 {
+  //    color: dodgerblue;        
+  //  }
+  //`]
+})
+export class AppComponent {
+}
